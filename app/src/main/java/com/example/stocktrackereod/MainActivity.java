a62b15a863a2e87501f4fff5e86f7add21c91ddf
Maurus Graf <maurus.graf@ergon.ch>
2023-03-15 15:48:01 +0100

    remove unused template, implement navigation between fragments, change strings

--- app/src/main/java/com/example/stocktrackereod/MainActivity.java
+++ app/src/main/java/com/example/stocktrackereod/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -36,12 +37,8 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+        binding.addNewPosition.setOnClickListener(view -> {
+            navController.navigate(R.id.SecondFragment);
         });
     }
 
